865d993cfbf55b9cc0a3a200f90b51bff64f156a
Wendy Santos <wendy.santossx@gmail.com>
2026-02-16 13:39:14 -0300

    Add files via upload

---
+++ Exercício 5 | Adicionando e removendo itens de uma lista/add_ou_remover_lista.py
@@ -0,0 +1,17 @@
+# ADICIONAR MAIS UM ITEM A LISTA
+
+print("\nAdicionando um item na lista\n")
+carrinho_de_mercado = ["arroz","feijao","tomate","biscoito"]
+print(carrinho_de_mercado)  #antes de adicionar
+
+item = carrinho_de_mercado.append("café")
+print(carrinho_de_mercado) #depois de adicionar
+
+
+#REMOVER UM ITEM DA LISTA
+
+print("\nRemovendo um item da lista\n")
+materiais = ["lápis","borracha","caneta"]
+print(materiais) #antes de remover
+item = materiais.remove("borracha")
+print(materiais) #depois de remover
